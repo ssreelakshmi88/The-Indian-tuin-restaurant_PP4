0e4d4c97452e816daeda2e2717b7312451b0ff65
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-10-07 20:53:34 +0000

    Corrected url in settings.py

--- indian_tuin/settings.py
+++ indian_tuin/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["indian_tuin.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['indian-tuin.herokuapp.com', "localhost"]
 
 
 # Application definition
