f9f7f1618305132042132ef9fc993fd4e3be15e0
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-10-15 23:01:39 +0000

    Add pagination links in menu

--- menu/views.py
+++ menu/views.py
@@ -5,6 +5,8 @@ from .models import Menu
 
 def menu(request):
     menu = Menu.objects.all()
-    context = {'menu': menu, }
+    paginator = Paginator(menu, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'menu': menu, 'page_obj': page_obj, }
     return render(request, 'menu/menu.html', context)
-
