b1957ec8a9ea2a4575bbe2e572f7438bc3663554
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-10-26 14:56:03 +0000

    Add testing views in blog app

---
+++ blog/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
+
+
+class TestBlogViews(TestCase):
+    """
+    This class tests blog app views.
+    """
+    def test_get_post_page(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_get_edit_blog_post_page(self):
+        item = Post.objects.create(
+            title='Masala milk',
+            author=User.objects.create(),
+            featured_image='',
+            excerpt='Milk prepared with spices',
+            meal_type=2,
+            food_type=3,
+            content='Some content'
+        )
+        response = self.client.get('blog/edit_post')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/edit_post.html')
+
--- menu/urls.py
+++ menu/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('create_menu/', views.create_menu_item, name='create_menu'),
     path('edit_menu/<slug:slug>', views.edit_menu_item, name='edit_menu'),
-    path('delete_menu/<slug:slug>', views.delete_menu_item, name='delete_menu'),
+    path('delete_menu/<slug:slug>',
+         views.delete_menu_item,
+         name='delete_menu'),
 ]
