540bab00495ceccc61aac268c377fdbbb7c13470
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2022-10-30 23:23:38 +0000

    Final check

--- indian_tuin/settings.py
+++ indian_tuin/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['indian-tuin.herokuapp.com', "localhost"]
 
@@ -109,11 +109,11 @@ WSGI_APPLICATION = 'indian_tuin.wsgi.application'
 #https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 #DATABASES = {
- #'default': {
-      #'ENGINE': 'django.db.backends.sqlite3',
-      #'NAME': BASE_DIR / 'db.sqlite3',
-     #}
- #}
+#'default': {
+    #'ENGINE': 'django.db.backends.sqlite3',
+    #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
