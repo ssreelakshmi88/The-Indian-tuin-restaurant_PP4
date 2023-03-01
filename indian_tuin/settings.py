00860de1d105ef7865a81b11baa34f780ae5a785
Sreelakshmi Sasidharan <sreelakshmi.s2@gmail.com>
2023-03-01 10:07:40 +0000

    Crispy-bootstrap 5 addeed to requirement.txt

--- indian_tuin/settings.py
+++ indian_tuin/settings.py
@@ -112,15 +112,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'indian_tuin.wsgi.application'
 
 
-#Database
-#https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#'default': {
+# DATABASES = {
+# 'default': {
     #'ENGINE': 'django.db.backends.sqlite3',
     #'NAME': BASE_DIR / 'db.sqlite3',
     #}
-#}
+# }
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
